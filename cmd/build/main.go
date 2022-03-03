13a177e2a5cc89bef10d353105ac2546c86403a7
Thomas <thomastayac@gmail.com>
2022-03-02 18:22:29 -0800

    implement bundle size tracking
    out/bundle-size.json now shows the bundle size

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"compress/gzip"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -14,12 +16,25 @@ func check(e error) {
 }
 
 func main() {
-	templateFilename := os.Args[1]
-	jsFilename := os.Args[2]
-	cssFilename := os.Args[3]
-	suggestionHTMLFilename := os.Args[4]
-	afterSuggestionsFilename := os.Args[5]
-	outFilename := os.Args[6]
+	subCommand := os.Args[1]
+	if subCommand == "template" {
+		Template()
+	} else if subCommand == "calcBundleSize" {
+		CalcBundleSize()
+	} else {
+		panic(fmt.Sprintf("Did not recognize subCommand %s", subCommand))
+	}
+}
+
+// Templating (the main behavior)
+
+func Template() {
+	templateFilename := os.Args[2]
+	jsFilename := os.Args[3]
+	cssFilename := os.Args[4]
+	suggestionHTMLFilename := os.Args[5]
+	afterSuggestionsFilename := os.Args[6]
+	outFilename := os.Args[7]
 
 	replaceMap := make(map[string][]byte, 2)
 	minifiedJS, err := os.ReadFile(jsFilename)
@@ -37,8 +52,6 @@ func main() {
 	afterSuggestions, err := os.ReadFile(afterSuggestionsFilename)
 	check(err)
 	replaceMap["<% AFTER_SUGGESTIONS %>"] = bytes.TrimSpace(afterSuggestions)
-
-	replaceMap["<% \\n %>"] = []byte("\n")
 
 	templateFile, err := os.Open(templateFilename)
 	check(err)
@@ -70,3 +83,36 @@ func main() {
 		panic(err)
 	}
 }
+
+// calculating bundle sizes
+func CalcBundleSize() {
+	// args
+	htmlFilename := os.Args[2]
+	outFilename := os.Args[3]
+
+	parsedData, err := os.ReadFile(htmlFilename)
+	check(err)
+
+	outFile, err := os.Create(outFilename)
+	check(err)
+	defer outFile.Close()
+	outWriter := bufio.NewWriter(outFile)
+	defer outWriter.Flush()
+
+	parsedSize := len(parsedData)
+
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+
+	_, err = zw.Write(parsedData)
+	check(err)
+
+	err = zw.Flush()
+	check(err)
+
+	gzippedSize := buf.Len()
+	// shitty version of json writer
+	toWrite := fmt.Sprintf(`{ "minified": %d, "parsed": %d }`, gzippedSize, parsedSize)
+	outWriter.WriteString(toWrite)
+}
+
