0bcfda9c07fbcb38b4d88a5c86a223b38f25d80f
Thomas <thomastayac@gmail.com>
2022-03-05 16:51:34 -0800

    write properly

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,13 +1,53 @@
 use wordle_helper::play_wordle;
 use wordle_helper::solution_words::SOLUTION_WORDS;
+use std::fmt;
+
+const MAX_GUESSES: usize = 32;
+#[derive(Debug)]
+struct FixedCountTable([u32; MAX_GUESSES]);
+
+impl FixedCountTable {
+    pub fn new() -> Self {
+        Self([0; MAX_GUESSES])
+    }
+
+    pub fn inc(&mut self, num_guesses: u8) {
+        let n: usize = usize::from(num_guesses);
+        assert!(n < MAX_GUESSES, "exceeded max guesses");
+        self.0[n] += 1;
+    }
+}
+impl fmt::Display for FixedCountTable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.0)
+        // TODO (angtay): use Miniserde or something instead of this shitty JSON stringify
+        /* f.write_str("{")?;
+        let mut first = true;
+        for i in 0..MAX_GUESSES {
+            let v = self.0[i];
+            if v > 0 {
+                if first {
+                    write!(f, "\"{}\": {}", i, v)?;
+                } else {
+                    write!(f, ", \"{}\": {}", i, v)?;
+                }
+                first = false;
+            }
+        }
+        f.write_str("}") */
+    }
+}
 
 fn main() {
-    let starting_word = SOLUTION_WORDS[0];
+    let starting_word = SOLUTION_WORDS[5];
+    let mut t = FixedCountTable::new();
     for word in SOLUTION_WORDS {
-        println!(
-            "Playing {} - num guesses {}",
-            word,
-            play_wordle(starting_word, word).num_guesses
-        );
+        let num_guesses = play_wordle(starting_word, word).num_guesses;
+        t.inc(num_guesses.try_into().expect("Less than 26 guesses"));
     }
+    println!(
+        "\"{}\": {}",
+        starting_word,
+        t
+    );
 }
